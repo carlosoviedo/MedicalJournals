f3e9fdb4e370cf3f3c16dddaa9ff11602d8a4879
Carlos Oviedo <carlos.oviedo@prodigious.com>
2016-03-17 21:57:54 -0500

    [hsql-db-setup] Added configuration for HSQL DB
    Added script for DB definition.

--- src/main/java/journal/Application.java
+++ src/main/java/journal/Application.java
@@ -2,12 +2,14 @@ package journal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  *
  * @author <a href="mailto:carlos.oviedo@gmail.com">Carlos Oviedo</a>
  */
 @SpringBootApplication
+@ImportResource("spring/data-layer.xml")
 public class Application {
 	public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
